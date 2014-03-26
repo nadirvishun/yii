ce313154da94b473484302f010ab2c8f8376fdb6
vishun nadir <vishun@VISHUN.(none)>
2014-03-26 22:23:00 +0800

    try to create front end

--- yii_cms/protected/modules/Admin/components/views/pageMenu.php
+++ yii_cms/protected/modules/Admin/components/views/pageMenu.php
@@ -1,6 +1,6 @@
 <ul>
-	<li><?php echo CHtml::link('导航添加', array('nav/create'));?></li>
-	<li><?php echo CHtml::link('导航管理', array('nav/admin'));?></li>
+	<li><?php echo CHtml::link('导航标签添加', array('nav/create'));?></li>
+	<li><?php echo CHtml::link('导航标签管理', array('nav/admin'));?></li>
 	<li><?php echo CHtml::link('轮播图片添加', array('banner/create'));?></li>
 	<li><?php echo CHtml::link('轮播图片管理', array('banner/admin'));?></li>
 	
